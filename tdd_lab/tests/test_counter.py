713f87c9bd500468da8fac2f6c4557b939d32916
Kemoshu <KevinR30010@gmail.com>
2025-02-04 21:26:30 -0800

    Student 5(Kevin): Implemented increment counter

--- tdd_lab/tests/test_counter.py
+++ tdd_lab/tests/test_counter.py
@@ -10,3 +10,28 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+import pytest
+from src import app
+from src import status
+
+@pytest.fixture()
+def client():
+    """Fixture for Flask test client"""
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndpoints:
+    """Test cases for Counter API"""
+
+    def test_increment_counter(self, client):
+        """It should increment an existing counter"""
+        
+        
+        create_response = client.post('/counters/test_counter')
+        assert create_response.status_code == 201 
+
+        increment_response = client.put('/counters/test_counter')
+        assert increment_response.status_code == 200 
+
+        data = increment_response.get_json()
+        assert data["test_counter"] == 1
