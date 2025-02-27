418d30083fedfa09891f19874c7de31a517c1b03
hhrh <hardykenta@icloud.com>
2025-02-27 14:59:50 -0800

    task2.1 after refactor

--- gen_ai/task2.1/HardyFenam_refactor_code.py
+++ gen_ai/task2.1/HardyFenam_refactor_code.py
@@ -2,25 +2,26 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
+        if not height:
+            return 0
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
         res = 0
 
-        l_max = [0] * n
-        r_max = [0] * n
-
-        l_max[0] = height[0]
-        for i in range(1, n):
-            l_max[i] = max(height[i], l_max[i-1])
-
-        r_max[n-1] = height[n-1]
-        for i in reversed(range(n-1)):
-            r_max[i] = max(height[i], r_max[i+1])
-
-        for i in range(n):
-            res += min(l_max[i], r_max[i]) - height[i]
+        while left < right:
+            if height[left] < height[right]:
+                left_max = max(left_max, height[left])
+                res += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(right_max, height[right])
+                res += right_max - height[right]
+                right -= 1
 
         return res
 
-print(Solution().trap([0,1,0,2,1,0,1,3,2,1,2,1])) # 6
-print(Solution().trap([4,2,0,3,2,5])) # 9
-print(Solution().trap([4,2,3])) # 1
+# Test cases
+print(Solution().trap([0,1,0,2,1,0,1,3,2,1,2,1]))  # Output: 6
+print(Solution().trap([4,2,0,3,2,5]))  # Output: 9
+print(Solution().trap([4,2,3]))  # Output: 1
